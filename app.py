ffb84675901d7f837424a2ec8227a61b3bf9c2bf
Taz Bruce <tasmanbrucework@gmail.com>
2022-11-25 16:18:13 +1300

    Added header

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, flash
 import pandas as pd
 import os
+import tempfile
 
 __author__ = 'Tasman Bruce'
 
 app = Flask(__name__)
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+APP_ROOT = tempfile.gettempdir()
 upload_ID = 1
 
 
 def convert_csv(csv_path):
-    quote_target = os.path.join(APP_ROOT, 'static/quote')
+    print('Processing CSV...')
+    quote_target = os.path.join(APP_ROOT, 'quote')
 
     if not os.path.isdir(quote_target):
+        print("No quote folder, creating now..")
         os.mkdir(quote_target)
 
-    output_dir = os.path.normpath(os.path.join(APP_ROOT, 'static/quote/PlumbingWorldQuote-' + str(upload_ID) + '.csv'))
+    print("Quote folder found")
+
+    output_dir = os.path.normpath(os.path.join(APP_ROOT, 'quote/PlumbingWorldQuote-' + str(upload_ID) + '.csv'))
+
+    print("Reading CSV..")
     file = pd.read_csv(csv_path)
     header = file.iloc[2]
     file = file[3:]
     file.columns = header
+
+    print("CSV Updated, saving now..")
     file.to_csv(output_dir, index=False)
     return send_file(output_dir, as_attachment=True)
 
@@ -34,16 +43,20 @@ def upload():
     try:
         global upload_ID
         upload_ID += 1
-        csv_target = os.path.join(APP_ROOT, 'static/csv')
+        csv_target = os.path.join(APP_ROOT, 'csv')
 
         # Preparing directory
         if not os.path.isdir(csv_target):
+            print("CSV folder not found, creating now...")
             os.mkdir(csv_target)
+
+        print("CSV folder found")
 
         # Uploading File
         for file in request.files.getlist('file'):
             filename = str(upload_ID) + "-" + file.filename
             destination = "/".join([csv_target, filename])
+            print("Uploading CSV...")
             file.save(destination)
 
             # Creating images
